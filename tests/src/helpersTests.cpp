9301bad810f1181fe48ae04ef46d495be66dd559
YevYar <kpyto2012@gmail.com>
2021-08-22 13:22:16 +0300

    Helpers tests

--- tests/src/helpersTests.cpp
+++ tests/src/helpersTests.cpp
@@ -1,5 +1,6 @@
-#include "../pch.h"
 #include <iostream>
+#include <exception>
+#include "../pch.h"
 
 #include "Helpers.h"
 
@@ -7,4 +8,40 @@ TEST(HelpersTests, isCinValidReturnFalse)
 {
 	std::cin.setstate(std::ios_base::eofbit);
 	EXPECT_FALSE(helpers::isCinValid());
+}
+
+TEST(HelpersTests, isCinValidThrowException)
+{
+	std::cin.setstate(std::ios_base::failbit);
+    EXPECT_THROW({
+        try
+        {
+            helpers::isCinValid();
+        }
+        catch (const std::exception& e)
+        {
+            // this tests that it has the correct message
+            EXPECT_STREQ("Reading input stream error.", e.what());
+            throw;
+        }
+    }, std::exception);
+
+    std::cin.setstate(std::ios_base::badbit);
+    EXPECT_THROW({
+        try
+        {
+            helpers::isCinValid();
+        }
+        catch (const std::exception& e)
+        {
+            EXPECT_STREQ("Reading input stream error.", e.what());
+            throw;
+        }
+        }, std::exception);
+}
+
+TEST(HelpersTests, isCinValidReturnTrue)
+{
+    std::cin.setstate(std::ios_base::goodbit);
+    EXPECT_TRUE(helpers::isCinValid());
 }
